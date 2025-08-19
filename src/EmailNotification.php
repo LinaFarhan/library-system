7a51d22d8d4aa550e8fdba3ecb3e53d90d80a2e0
LINA FARHAN <51128456+LinaFarhan@users.noreply.github.com>
2025-08-19 18:19:26 +0300

    Update EmailNotification.php

--- src/EmailNotification.php
+++ src/EmailNotification.php
@@ -3,10 +3,10 @@ namespace LibrarySystem;
 
 class EmailNotification implements NotificationInterface {
     public function send(string $message, string $recipient): void {
-<<<<<<< HEAD
+ 
         echo "OK send $recipient: $message<br>";
-=======
-        echo "✉️ تم إرسال بريد إلى $recipient: $message<br>";
->>>>>>> 50c214098081ecdfe1af2b9d6688143b23bc9b52
+ 
+        echo "ok send $recipient: $message<br>";
+ 
     }
 }
