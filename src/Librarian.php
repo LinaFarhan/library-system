e4d86f4a1f6dd246f289f3b7e044adc30e3bc399
LINA FARHAN <51128456+LinaFarhan@users.noreply.github.com>
2025-08-19 18:19:52 +0300

    Update Librarian.php

--- src/Librarian.php
+++ src/Librarian.php
@@ -8,10 +8,8 @@ class Librarian extends User {
     }
 
     public function interactWithLibrary() {
-<<<<<<< HEAD
+ 
         return "📖 {$this->name}  can add or remove book";
-=======
-        return "📖 {$this->name} (أمين المكتبة) يمكنه إضافة وحذف الكتب.";
->>>>>>> 50c214098081ecdfe1af2b9d6688143b23bc9b52
+ 
     }
 }
